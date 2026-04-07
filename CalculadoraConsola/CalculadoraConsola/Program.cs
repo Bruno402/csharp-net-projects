98ae27dca24858d16bad5ebacfb56c5143cb22d7
Bruno Gomez <71837792+Bruno402@users.noreply.github.com>
2026-04-07 12:39:14 -0600

    Add files via upload

---
+++ CalculadoraConsola/CalculadoraConsola/Program.cs
@@ -0,0 +1,54 @@
+﻿double resultado = 0;
+string regresar = "SI";
+
+while (regresar == "SI")
+{
+    Console.WriteLine("=== Calculadora en C# ===");
+    Console.WriteLine("Selecciona una operación:");
+    Console.WriteLine("1. Suma");
+    Console.WriteLine("2. Resta");
+    Console.WriteLine("3. Multiplicación");
+    Console.WriteLine("4. División");
+
+    Console.Write("Opción: ");
+    int opcion = int.Parse(Console.ReadLine());
+
+    if( opcion >= 1 && opcion < 5)
+    {
+        Console.Write("Ingresa el primer número: ");
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Ingresa el segundo número: ");
+        double num2 = double.Parse(Console.ReadLine());
+        switch (opcion)
+        {
+            case 1:
+                resultado = num1 + num2;
+                break;
+            case 2:
+                resultado = num1 - num2;
+                break;
+            case 3:
+                resultado = num1 * num2;
+                break;
+            case 4:
+                if (num2 != 0)
+                    resultado = num1 / num2;
+                else
+                    Console.WriteLine("Error: División entre cero no permitida.");
+                break;
+        }
+        Console.WriteLine($"Resultado: {resultado}");
+    }
+    else
+    {
+        Console.WriteLine("La opcion ingresada no esta en la calculadora :( ");
+    }
+    
+    Console.WriteLine("Si quieres realizar otra operacion introduce SI, de lo contrario introduce NO...");
+    regresar = Console.ReadLine();
+    Console.Clear();
+}
+
+Console.WriteLine("¡Gracias por ocupar esta calculadora amigo vuelve pronto!");
+Console.ReadKey();
---
+++ CalculadoraConsola/CalculadoraConsola/README.md
@@ -0,0 +1,12 @@
+﻿# Calculadora en C#
+
+Proyecto sencillo de consola en C# que permite realizar operaciones básicas:
+- Suma
+- Resta
+- Multiplicación
+- División
+
+## Cómo ejecutar
+1. Clonar el repositorio
+2. Abrir en Visual Studio
+3. Ejecutar con `Ctrl + F5`
